d16ff8726461daa23e37c43ac211ba54e8d2f737
Md. Shamim Rahman <apubdtea@gmail.com>
2025-07-09 18:42:53 +0600

    feat: define available slots api
    Writting available_slots_api to return appointment slots to users. It
    checks date, staff id, service id to ensure all of them are correct else
    it raises exception and gives feedback to end user.
    
    There are also few imports in here the timezone module from django,
    Staff and Service from the booking model and the original get_available_slots function from the utility.

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,9 @@
-from django.http import JsonResponse
 from datetime import datetime
-from booking.utils import get_available_staff
+from django.http import JsonResponse
+from django.utils import timezone
+
+from booking.utils import get_available_staff, get_available_slots
+from booking.models import Staff, Service
 
 
 def available_staff_api(request):
@@ -23,3 +26,29 @@ def available_staff_api(request):
     data = list(staff_qs.values("id", "name"))
 
     return JsonResponse({"staff": data})
+
+
+def available_slots_api(request):
+    date_str = request.GET.get("date")
+    staff_id = request.GET.get("staff_id")
+    service_id = request.GET.get("service_id")
+
+    if not (date_str and staff_id and service_id):
+        return JsonResponse({"error": "Missing required parameters"}, status=400)
+
+    try:
+        date_obj = datetime.strptime(date_str, "%Y-%m-%d").date()
+    except ValueError:
+        return JsonResponse({"error": "Invalid date format, use YYYY-MM-DD"}, status=400)
+
+    if date_obj < timezone.localdate():
+        return JsonResponse({"slots": []})
+
+    try:
+        staff = Staff.objects.get(id=staff_id)
+        service = Service.objects.get(id=service_id)
+    except (Staff.DoesNotExist, Service.DoesNotExist):
+        return JsonResponse({"error": "Invalid staff or service ID"}, status=404)
+
+    slots = get_available_slots(date_obj, service.id, staff)
+    return JsonResponse({"slots": slots})
