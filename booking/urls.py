a3cf2812bb7e3edadf40cfcdbe72afe875d46740
Md. Shamim Rahman <apubdtea@gmail.com>
2025-07-17 22:31:06 +0600

    chore: comment booking urls

--- booking/urls.py
+++ booking/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from booking import views
 
 urlpatterns = [
+    # Template urls
     path("book/", views.booking_page, name="booking-page"),
     # My custom apis
     path("api/available-staff/", views.available_staff_api, name="available-staff-api"),
