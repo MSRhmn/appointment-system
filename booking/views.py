b0d44d9241f76a9888520ed7fa53df0fea526d66
Md. Shamim Rahman <apubdtea@gmail.com>
2025-07-17 23:23:56 +0600

    update: booking views available slots api
    Removing staff selection from the program, internally adding/assigning
    staff to services, it only show read only staff data to the user.

--- booking/views.py
+++ booking/views.py
@@ -36,10 +36,9 @@ def available_staff_api(request):
 
 def available_slots_api(request):
     date_str = request.GET.get("date")
-    staff_id = request.GET.get("staff_id")
     service_id = request.GET.get("service_id")
 
-    if not (date_str and staff_id and service_id):
+    if not (date_str and service_id):
         return JsonResponse({"error": "Missing required parameters"}, status=400)
 
     try:
@@ -53,13 +52,23 @@ def available_slots_api(request):
         return JsonResponse({"slots": []})
 
     try:
-        staff = Staff.objects.get(id=staff_id)
         service = Service.objects.get(id=service_id)
-    except (Staff.DoesNotExist, Service.DoesNotExist):
-        return JsonResponse({"error": "Invalid staff or service ID"}, status=404)
+    except Service.DoesNotExist:
+        return JsonResponse({"error": "Invalid service ID"}, status=404)
 
-    slots = get_available_slots(date_obj, service.id, staff)
-    return JsonResponse({"slots": slots})
+    # Assign the first available staff
+    available_staff = get_available_staff(date_obj).first()
+
+    if not available_staff:
+        return JsonResponse({"slots": [], "staff": None})
+
+    slots = get_available_slots(date_obj, service.id, available_staff)
+    return JsonResponse(
+        {
+            "slots": slots,
+            "staff": {"id": available_staff.id, "name": available_staff.name},
+        }
+    )
 
 
 @csrf_exempt
