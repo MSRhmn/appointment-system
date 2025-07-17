e10afc795a67678a3dba6d41709756946955e1d7
Md. Shamim Rahman <apubdtea@gmail.com>
2025-07-17 23:26:51 +0600

    update: removing staff api

--- booking/views.py
+++ booking/views.py
@@ -12,28 +12,6 @@ from booking.models import Staff, Service
 
 
 # apis
-def available_staff_api(request):
-    """
-    API to return available staff for a given date.
-    """
-    date_str = request.GET.get("date")
-
-    if not date_str:
-        return JsonResponse({"error": "Missing date parameter"}, status=400)
-
-    try:
-        date_obj = datetime.strptime(date_str, "%Y-%m-%d").date()
-    except ValueError:
-        return JsonResponse(
-            {"error": "Invalid date format, use YYYY-MM-DD"}, status=400
-        )
-
-    staff_qs = get_available_staff(date_obj)
-    data = list(staff_qs.values("id", "name"))
-
-    return JsonResponse({"staff": data})
-
-
 def available_slots_api(request):
     date_str = request.GET.get("date")
     service_id = request.GET.get("service_id")
