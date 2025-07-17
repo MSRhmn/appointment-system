0fe6a594f23910f5db83d5df46052c636153d4e2
Md. Shamim Rahman <apubdtea@gmail.com>
2025-07-17 23:27:40 +0600

    remove: staff api url

--- booking/urls.py
+++ booking/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     # Template urls
     path("book/", views.booking_page, name="booking-page"),
     # My custom apis
-    path("api/available-staff/", views.available_staff_api, name="available-staff-api"),
     path("api/available-slots/", views.available_slots_api, name="available-slots-api"),
     path(
         "api/book-appointment/", views.book_appointment_api, name="book-appointment-api"
