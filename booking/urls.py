0df30f389594901721a772032e52dbd323068f56
Md. Shamim Rahman <apubdtea@gmail.com>
2025-07-17 22:30:09 +0600

    urls: define booking page url

--- booking/urls.py
+++ booking/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from booking import views
 
 urlpatterns = [
+    path("book/", views.booking_page, name="booking-page"),
+    # My custom apis
     path("api/available-staff/", views.available_staff_api, name="available-staff-api"),
     path("api/available-slots/", views.available_slots_api, name="available-slots-api"),
     path(
