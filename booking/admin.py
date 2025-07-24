3a12cbf75061be7ea219a72c12cb098e243f1df3
Md. Shamim Rahman <apubdtea@gmail.com>
2025-07-24 11:40:34 +0600

    update: booking admin
    Making the BookingAdmin created_at attribute read only. So staff doesn't
    accidentally change the time.

--- booking/admin.py
+++ booking/admin.py
@@ -27,3 +27,4 @@ class BookingAdmin(admin.ModelAdmin):
     list_display = ("customer_name", "service", "staff", "date", "start_time")
     list_filter = ("date", "staff", "service")
     search_fields = ("customer_name", "customer_email")
+    readonly_fields = ("created_at",)
